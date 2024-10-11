d2ba80f1f27713ef445458a673da1aaad92c7ad3
FUNNGOD0 <okrizal98@gmail.com>
2024-10-11 21:57:24 +0800

    2

--- font1.1.py
+++ font1.1.py
@@ -2,8 +2,8 @@ import PySimpleGUI as sg
 sg.theme("DarkGreen4")
 sg.theme_text_color("#FFFF00")
 window = sg.Window(title="Profile",
-                    layout=[[sg.Text("NPM   : 2110010461 ")],
-                            [sg.Text("Nama  : MUHAMMAD NUR IMANNUDIN ")],
+                    layout=[[sg.Text("NPM   : 2210010177 ")],
+                            [sg.Text("Nama  : MUHAMMAD RIZAL ABDI ")],
                             [sg.Text("Kelas : 5N Regular Banjarmasin ")]],
                     size=(400,200),
                     font=("Times", 18))
--- font1.3.py
+++ font1.3.py
@@ -3,8 +3,8 @@ sg.theme("DarkGreen4")
 sg.theme_text_color("#FFFF00")
 window = sg.Window(title="Profile",
                     layout=[[sg.Text("SELAMAT DATANG DI KELAS",font=("Arial",25,"italic","bold","underline"))],
-                            [sg.Text("NPM       : 2110010461 ")],
-                            [sg.Text("Nama      : MUHAMMAD NUR IMANNUDIN ")],
+                            [sg.Text("NPM       : 2210010177 ")],
+                            [sg.Text("Nama      : MUHAMMAD RIZAL ABDI ")],
                             [sg.Text("Kelas     : 5N Regular Banjarmasin ")]],
                     size=(510,200),
                     font=("Times", 18))
